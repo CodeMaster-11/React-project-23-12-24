f11c0ce1b31493bce9018e8bf1ff04f1910a6923
CodeMaster <kskhedkar1105@gmail.com>
2025-01-02 15:48:10 +0530

    intial commit

---
+++ myapp9/src/App.js
@@ -0,0 +1,15 @@
+import { Component } from 'react'
+import Coma from './comA'
+
+class App extends Component{
+  render(){
+    return (
+      <>
+      <h1>Hello</h1>
+    <Coma></Coma>
+    </>
+    )
+  }
+}
+
+export default App
---
+++ myapp9/src/comA.js
@@ -0,0 +1,8 @@
+import { Component } from "react"
+
+class  Coma extends Component{
+    render(){
+        return<h1>Brother</h1>
+    }
+}
+export default Coma
